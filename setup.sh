ad57b1d460386e59e15661169f178bcf8a1fdd40
Açal HABI <Acal.habi@canal-plus.com>
2023-12-12 17:08:50 +0100

    use pm2 instead

--- setup.sh
+++ setup.sh
@@ -14,12 +14,12 @@ configFile="bot.config.example"
 
 cd "$(dirname "$0")"
 apt-get update
-apt-get install -y python3 python3-pip curl supervisor
+apt-get install -y python3 python3-pip curl nodejs gcc g++ make
 pip3 install -r requirements.txt
+npm install -g pm2
 cp $configFile bot.config
 sed -i "s/\(tokenBot=\).*/\1$tokenBot/" bot.config
 sed -i "s/\(guildId=\).*/\1$guildId/" bot.config
 sed -i "s/\(channelId=\).*/\1$channelId/" bot.config
 chmod +x main.py
-mkdir -p /var/log/supervisord/
-supervisord -c supervisord.conf
+pm2 start /app/Amazon-Watch-Price-Bot/main.py
