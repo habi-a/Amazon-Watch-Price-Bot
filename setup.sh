93896ffe3c92fbdfb8347ddb7707c5b70d620476
Açal HABI <Acal.habi@canal-plus.com>
2023-12-12 17:39:41 +0100

    add pm2 startup

--- setup.sh
+++ setup.sh
@@ -13,14 +13,15 @@ channelId="$3"
 configFile="bot.config.example"
 
 cd "$(dirname "$0")"
-apt-get update
-apt-get install -y python3 python3-pip curl nodejs gcc g++ make
-pip3 install -r requirements.txt
-npm install -g pm2
 cp $configFile bot.config
 sed -i "s/\(tokenBot=\).*/\1$tokenBot/" bot.config
 sed -i "s/\(guildId=\).*/\1$guildId/" bot.config
 sed -i "s/\(channelId=\).*/\1$channelId/" bot.config
 chmod +x main.py
+apt-get update
+apt-get install -y python3 python3-pip curl nodejs gcc g++ make
+pip3 install -r requirements.txt
+npm install -g pm2
+pm2 startup
 pm2 start "/usr/bin/python3 /app/Amazon-Watch-Price-Bot/main.py" --name "Amazon-watch-price-bot"
 pm2 save
