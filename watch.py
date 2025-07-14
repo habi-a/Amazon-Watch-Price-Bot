28f95330cd50be74d49840b57829f119f9759e0e
Açal Habi <34644835+habi-a@users.noreply.github.com>
2025-07-14 21:09:46 +0200

    Update watch.py

--- watch.py
+++ watch.py
@@ -6,18 +6,18 @@ from utils import convert_price_to_number, HOUR
 async def watch_background(bot, watch_list):
     while True:
         await asyncio.sleep(12 * HOUR)
-        for item in watch_list:
-            message = ""
-            price_today = get_price(item["link"])
-            price_today_float = convert_price_to_number(price_today)
-            price_stored_float = convert_price_to_number(item["price"])
-
-            if (price_today_float != price_stored_float):
-                message = "Price for: " + item["title"] + " has changed\n"
-                message += "Old price: " + item["price"] + "\t New price: " + price_today
-                item["price"] = price_today
-                user = bot.get_user(item["user_id"])
-                if user:
-                    await user.send(message)
-                else:
-                    print("no user found to send alert")
+        for user_id, items in watch_list.items():
+            for item in items:
+                price_today = get_price(item["link"])
+                price_today_float = convert_price_to_number(price_today)
+                price_stored_float = convert_price_to_number(item["price"])
+                if price_today_float != price_stored_float:
+                    item["price"] = price_today
+                    try:
+                        await user.send(
+                            f"Prix modifié pour **{item['title']}**\n"
+                            f"Ancien prix : {item['price']}\n"
+                            f"Nouveau prix : {price_today}"
+                        )
+                    except discord.Forbidden:
+                        print(f"Impossible d'envoyer un DM à {user_id}")
